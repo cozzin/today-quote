378cabbff9ce4b41a57890324ee63c061838664a
Ernest Hong <hsh3592@gmail.com>
2023-01-19 00:21:09 +0900

    Create project

---
+++ src/main/kotlin/com/cozzin/wisesaying/WiseSayingApplication.kt
@@ -0,0 +1,14 @@
+package com.cozzin.wisesaying
+
+import org.springframework.boot.Banner
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WiseSayingApplication
+
+fun main(args: Array<String>) {
+	runApplication<WiseSayingApplication>(*args) {
+		setBannerMode(Banner.Mode.OFF)
+	}
+}
---
+++ src/test/kotlin/com/cozzin/wisesaying/WiseSayingApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.cozzin.wisesaying
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WiseSayingApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
