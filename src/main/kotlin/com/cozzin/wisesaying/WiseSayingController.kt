3682f0d5c9c90987fe833c0e50c707c0b3bef9f5
Ernest Hong <hsh3592@gmail.com>
2023-02-02 02:03:37 +0900

    Add rest controller

--- src/main/kotlin/com/cozzin/wisesaying/WiseSayingController.kt
+++ src/main/kotlin/com/cozzin/wisesaying/WiseSayingController.kt
@@ -1,12 +1,12 @@
 package com.cozzin.wisesaying
 
-import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.ui.set
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RestController
 
-@Controller
-class HtmlController {
+@RestController
+class WiseSayingController {
 
     @GetMapping("/")
     fun blog(model: Model): String {
